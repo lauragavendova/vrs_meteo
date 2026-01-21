edb4c919829b4644a193d57e3ac47969e35efdc4
Laura Gavendová <lauragavendova@gmail.com>
2026-01-21 17:47:22 +0100

    obnovenie main

--- launch_meteo/meteo/Core/Src/main.c
+++ launch_meteo/meteo/Core/Src/main.c
@@ -24,6 +24,7 @@
 #include "tim.h"
 #include "usart.h"
 #include "gpio.h"
+#include <stdio.h>
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -57,8 +58,13 @@ int8_t screen_index = 0;
 int8_t screen_status = 0;
 int8_t screen_tick = 0;
 int32_t butonCounter = 0;
-int8_t mode = 0;
 char string_buf[7];
+
+
+int8_t mode = 1;
+uint8_t screen = 0;
+uint32_t change = 0;
+volatile uint8_t btn_pressed = 0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -108,29 +114,104 @@ int main(void)
   MX_TIM7_Init();
   MX_TIM6_Init();
   /* USER CODE BEGIN 2 */
+
+  char* big[4] = {"10:00", "22.1", "50", "1013"};
+  char* small[4] = {":00", "C", "%", "hPa"};
+  char* line2[4] = {"date", "", "OK", ""};
+
+  ILI9341_Reset();
   HAL_Delay(10);
   ILI9341_Init();
   HAL_Delay(50);
 
-  ILI9341_FillScreen(BLACK); //fill whole screen with black color
+  ILI9341_SetRotation(3);
+  ILI9341_FillScreen(BGCOLOR);
 
   HAL_TIM_Base_Start_IT(&htim6);
 
+  if(mode==0){
+      DrawDataCentered_WithOffset("Welcome!", FONT4, 2, 75, FCOLOR);
+      DrawDataCentered_WithOffset("Creators: Bodor, Gavendova,", FONT4, 1, 125, FCOLOR);
+      DrawDataCentered_WithOffset("Kapina, Krajmer", FONT4, 1, 125 + FONT4[2] + 1, FCOLOR);
+      DrawDataCentered_WithOffset("*Press the button to stop the screen", FONT4, 1, 240 - FONT4[2] - 1, RED);
+      HAL_Delay(3000);
 
-  //test printout
-  /*ILI9341_DrawRectangle(0, 0, 320, 30, BLUE);
-  ILI9341_DrawText("WEATHER STATION", FONT4, 60, 8, WHITE, BLUE);
+      ILI9341_FillScreen(BGCOLOR);
 
-    ILI9341_DrawHollowRectangleCoord(10, 40, 310, 75, WHITE);
-  	ILI9341_DrawText("Temperature:        *C", FONT4, 50, 50, WHITE, BLACK);
-  	digit_to_ascii_XX_X(temperature, string_buf);
-  	ILI9341_DrawText(string_buf, FONT4, 190, 50, WHITE, BLACK);
+      uint32_t last_sim_update = 0;
+      int16_t sim_pressure = 1010;
+      int16_t last_displayed_pressure = -1;
 
-  	ILI9341_DrawHollowRectangleCoord(10, 85, 310, 120, WHITE);
-  	ILI9341_DrawText("Pressure:           kPa", FONT4, 50, 95, WHITE, BLACK);
-  	digit3_to_ascii(pressure, string_buf);
-  	ILI9341_DrawText(string_buf, FONT4, 190, 95, WHITE, BLACK);
-*/
+      while (1) {
+          if (!btn_pressed && (HAL_GetTick() - change >= 5000)) {
+              screen++;
+              if (screen > 5) screen = 0;
+              change = HAL_GetTick();
+
+              ILI9341_FillScreen(BGCOLOR);
+
+              // Pri prepnutí obrazovky resetujeme "last" hodnotu, aby sa hneď vykreslila nová
+              last_displayed_pressure = -1;
+
+              switch (screen) {
+                  case 0: DrawSummary(big, small, line2, FONT4);  break;
+                  case 1: DrawDataCentered2("10:00", ":00", "date", FONT4, 5, 3, 3); break;
+                  case 2: DrawDataCentered2("22.1", "C","", FONT4, 5, 3, 0);    break;
+                  case 3: DrawDataCentered2("50", "%", "OK", FONT4, 5, 3, 3);      break;
+                  case 4: break;
+                  case 5: DrawSun(YELLOW); break;
+              }
+          }
+
+          if (HAL_GetTick() - last_sim_update >= 500) {
+              last_sim_update = HAL_GetTick();
+              sim_pressure++;
+              if (sim_pressure > 1030) sim_pressure = 990;
+          }
+
+          //dynamic
+
+          if (sim_pressure != last_displayed_pressure) {
+
+              sprintf(string_buf, "%d", sim_pressure);
+
+              if (screen == 4) {
+                  DrawDataCentered2(string_buf, "hPa", "", FONT4, 5, 3, 0);
+              }
+              else if (screen == 0) {
+                  big[3] = string_buf;
+                  DrawSummary(big, small, line2, FONT4);
+              }
+              last_displayed_pressure = sim_pressure;
+          }
+      }
+    }
+
+  if(mode==1){
+    DrawDataCentered_WithOffset("Welcome!", FONT4, 2, 75, FCOLOR);
+    DrawDataCentered_WithOffset("Creators: Bodor, Gavendova,", FONT4, 1, 125, FCOLOR);
+    DrawDataCentered_WithOffset("Kapina, Krajmer", FONT4, 1, 125 + FONT4[2] + 1, FCOLOR);
+    DrawDataCentered_WithOffset("*Press the button to switch screens", FONT4, 1, 240 - FONT4[2] - 1, RED);
+
+
+    while (1) {
+        if (btn_pressed) {
+            screen++;
+            if (screen > 5) screen = 0;
+
+            ILI9341_FillScreen(BGCOLOR);
+            switch (screen) {
+                      	  case 0: DrawSummary(big, small, line2, FONT4);  break;
+                          case 1: DrawDataCentered2("10:00", ":00", "date", FONT4, 5, 3, 3); break;
+                          case 2: DrawDataCentered2("22.1", "C","", FONT4, 5, 3, 0);    break;
+                          case 3: DrawDataCentered2("50", "%", "OK", FONT4, 5, 3, 3);      break;
+                          case 4: DrawDataCentered2("1013", "hPa","", FONT4, 5, 3,0);  break;
+                          case 5: DrawSun(YELLOW); break;
+                      }
+            }
+            btn_pressed = 0;
+        }
+    }
 
   /* USER CODE END 2 */
 
@@ -141,50 +222,42 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-if (mode == 0) {
-	  if (screen_status) {
-		  if (screen_index >= 3) {
-			  screen_index = 0;
-		  }
-		  if (screen_index == 0) {
-			  ILI9341_FillScreen(BLUE);
-		  }
-		  else if (screen_index == 1) {
-			  ILI9341_FillScreen(RED);
-		  }
-		  else if (screen_index == 2) {
-			  ILI9341_FillScreen(WHITE);
-		  }
-		  screen_status = 0;
-		  screen_index++;
-	  }
-}
-if (mode == 1) {
-	if (screen_tick) {
-		if (screen_index >= 3) {
-			screen_index = 0;
-		}
-		if (screen_index == 0) {
-		    ILI9341_FillScreen(BLUE);
-		}
-		else if (screen_index == 1) {
-		    ILI9341_FillScreen(RED);
-		}
-		else if (screen_index == 2) {
-		    ILI9341_FillScreen(WHITE);
-		}
-		screen_tick = 0;
-		screen_index++;
-	}
-}
-	  /*if (screen_status) {
-		  pressure++;
-		  digit3_to_ascii(pressure, string_buf);
-		  ILI9341_DrawText(string_buf, FONT4, 190, 95, WHITE, BLACK);
-		  screen_status = 0;
-	  }*/
-
-
+//if (mode == 0) {
+//	  if (screen_status) {
+//		  if (screen_index >= 3) {
+//			  screen_index = 0;
+//		  }
+//		  if (screen_index == 0) {
+//			  ILI9341_FillScreen(BLUE);
+//		  }
+//		  else if (screen_index == 1) {
+//			  ILI9341_FillScreen(RED);
+//		  }
+//		  else if (screen_index == 2) {
+//			  ILI9341_FillScreen(WHITE);
+//		  }
+//		  screen_status = 0;
+//		  screen_index++;
+//	  }
+//}
+//if (mode == 1) {
+//	if (screen_tick) {
+//		if (screen_index >= 3) {
+//			screen_index = 0;
+//		}
+//		if (screen_index == 0) {
+//		    ILI9341_FillScreen(BLUE);
+//		}
+//		else if (screen_index == 1) {
+//		    ILI9341_FillScreen(RED);
+//		}
+//		else if (screen_index == 2) {
+//		    ILI9341_FillScreen(WHITE);
+//		}
+//		screen_tick = 0;
+//		screen_index++;
+//	}
+//}
 	  /*HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
 	  HAL_Delay(500);
 	  HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET);
@@ -238,7 +311,14 @@ void SystemClock_Config(void)
 }
 
 /* USER CODE BEGIN 4 */
-
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+  if (GPIO_Pin == BTN_Pin)
+  {
+	btn_pressed = !btn_pressed;
+	HAL_GPIO_TogglePin(GPIOB, LD3_Pin);
+  }
+}
 /* USER CODE END 4 */
 
 /**
